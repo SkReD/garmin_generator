2900977e14cdaffd9e30282ef48f6ca8b4d90a0d
SkReD <mshipov@yandex.ru>
2018-02-28 06:59:50 +0300

    fix(distanceMeasure): поправил расчет длины пути
    Не конвертировались гео координаты в радианы

--- src/distanceMeasure.js
+++ src/distanceMeasure.js
@@ -1,11 +1,19 @@
-module.exports.measureDistance = function measureDistance(startLatitude, startLongitude, endLatitude, endLongitude) {  // generally used geo measurement function
-    const R = 6378.137; // Radius of earth in KM
-    const dLat = endLatitude * Math.PI / 180 - startLongitude * Math.PI / 180;
-    const dLon = endLongitude * Math.PI / 180 - startLongitude * Math.PI / 180;
+module.exports.measureDistance = function measureDistance(lat1, lon1, lat2, lon2) {  // generally used geo measurement function
+    const R = 6371; // km
+    const dLat = toRad(lat2 - lat1);
+    const dLon = toRad(lon2 - lon1);
+    lat1 = toRad(lat1);
+    lat2 = toRad(lat2);
+
     const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.cos(startLongitude * Math.PI / 180) * Math.cos(endLatitude * Math.PI / 180) *
-        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+        Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
     const d = R * c;
-    return d * 1000; // meters
+    return d * 1000;
 };
+
+// Converts numeric degrees to radians
+function toRad(Value)
+{
+    return Value * Math.PI / 180;
+}
